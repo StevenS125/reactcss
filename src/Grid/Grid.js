df468f8a5c3cb4b338c2b46fc20eae9d82ede907
StevenS125 <sts12589@gmail.com>
2018-03-19 16:16:41 -0400

    built project in react of Nasa space shuttle facts

---
+++ src/Grid/Grid.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import cx from 'classnames';
+import Griditem from "./Griditem";
+import styles from "./Grid.module.css";
+import mediaStyles from './Media.module.css';
+
+export default class Grid extends React.Component {
+updateClicked(id) {
+  this.props.updateClicked(id)
+}
+
+  renderFacts() {
+    return this.props.facts.map(item => 
+    <Griditem  
+    key={item.id}
+    fact={item}
+    updateClicked={this.updateClicked.bind(this)}
+    />)
+  }
+
+  render() {
+    return (
+      <section className={cx(styles.grid, mediaStyles.grid)}>
+        {this.renderFacts()}
+      </section>
+    );
+  }
+}
